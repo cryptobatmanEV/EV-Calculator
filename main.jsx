f2766ee3e2bded4bf8bd02a83d7a4042449345e9
cryptobatmanEV <tybatman94@gmail.com>
2026-04-13 13:14:30 -0500

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import EVCalculator from "./ev-calculator.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <EVCalculator />
+  </React.StrictMode>
+);
